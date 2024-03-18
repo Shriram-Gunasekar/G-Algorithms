2d1b5f04ab98b7da2078284d025ef780e32f3b26
shriramg <gshriram14@gmail.com>
2024-03-18 12:07:47 +0530

    Create spcrossover.py

---
+++ general/spcrossover.py
@@ -0,0 +1,5 @@
+def single_point_crossover(parent1, parent2):
+    crossover_point = random.randint(1, len(parent1) - 1)
+    child1 = parent1[:crossover_point] + parent2[crossover_point:]
+    child2 = parent2[:crossover_point] + parent1[crossover_point:]
+    return child1, child2
