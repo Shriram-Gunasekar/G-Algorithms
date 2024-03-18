661b7bc06039013e0f5d047052047b371448b670
shriramg <gshriram14@gmail.com>
2024-03-18 12:07:34 +0530

    Update mutate.py

--- general/mutate.py
+++ general/mutate.py
@@ -1,1 +1,6 @@
+def mutate(individual, mutation_rate):
+    for i in range(len(individual)):
+        if random.random() < mutation_rate:
+            individual[i] = 1 - individual[i]  # Flip the bit
+    return individual
 
